3cd89964a460e04a079b09fb6a9f630772b80801
andre <matei.i.andreesc@gmail.com>
2026-05-23 17:39:49 +0300

    Add: functie cmmdc, adunare, inmultire

--- src/mark.h
+++ src/mark.h
@@ -7,7 +7,9 @@ typedef struct
 }grafic;
 
 
-
+int cmmdc(int a, int b);
+grafic adunare(grafic a, grafic b);
+grafic inmultire(grafic a, grafic b);
 int convert(double value, double d);
 void citire_fis(int *N, double *d, int *k, int *p_start, int *p_target, int **values, FILE *infile, int *maxim, int *minim);
 void creare_grafic(grafic **mark, const int *values, int N, int n, int minim);
